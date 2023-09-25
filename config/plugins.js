2210e3cb886cfcda84afe5da866fca078f9684f3
Naol Melesse <naolmelesse91@gmail.com>
2023-09-25 21:11:09 +0300

    debug

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+          cloud_name: env('dgfd4wb8d'),
+          api_key: env('928747199988742'),
+          api_secret: env('3V5Us7nJiTa3EUIeGpp5ePOzTOQ'),
         },
         actionOptions: {
           upload: {},
