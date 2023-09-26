78b3b3850b9e5641614311b46dbf429a86177b23
Naol Melesse <naolmelesse91@gmail.com>
2023-09-26 12:00:51 +0300

    enabled graphql playground

--- config/plugins.js
+++ config/plugins.js
@@ -14,5 +14,16 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    // ...
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
   });
